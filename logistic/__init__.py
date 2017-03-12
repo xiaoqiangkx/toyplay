76ea53660852bbf95ba61782960ab77ea2461350
coolman <xiaoqiangkx@gmail.com>
2017-03-12 23:47:37 +0800

    first commit in toyplay

---
+++ logistic/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/env python
+# encoding: utf-8
+
+
+"""
+@version: 1.0
+@author: xiaoqiangkx
+@file: __init__.py.py
+@time: 2017/3/12 23:46
+@change_time:
+1.2017/3/12 23:46
+"""
+
+if __name__ == '__main__':
+    pass
---
+++ logistic/logistic.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/env python
+# encoding: utf-8
+
+
+"""
+@version: 1.0
+@author: xiaoqiangkx
+@file: logistic.py
+@time: 2017/3/12 23:47
+@change_time:
+1.2017/3/12 23:47
+"""
+
+if __name__ == '__main__':
+    pass
