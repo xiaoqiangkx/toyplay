746788a609d537119b510794ff3fd1d0880fe5b5
coolman <xiaoqiangkx@gmail.com>
2017-04-13 22:53:12 +0800

    add ctrip test1

---
+++ network/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+
+"""
+@version: 1.0
+@author: clark
+@file: __init__.py.py
+@time: 2017/3/19 17:24
+@change_time:
+1.2017/3/19 17:24
+"""
+
+if __name__ == '__main__':
+    pass
