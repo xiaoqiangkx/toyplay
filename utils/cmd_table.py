ff0ff014ae7b17de7bd451b810898b5415859128
coolman <xiaoqiangkx@gmail.com>
2017-05-01 14:22:06 +0800

    add data analyse

---
+++ utils/cmd_table.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+
+"""
+@version: 1.0
+@author: clark
+@file: cmd_table.py
+@time: 2017/4/30 13:12
+@change_time:
+1.2017/4/30 13:12
+"""
+
+data = """
+PassengerId	Survived	Pclass	Name	Sex	Age	SibSp	Parch	Ticket	Fare	Cabin	Embarked
+0	1	0	3	Braund,Mr. Owen Harris	male	22.0	1	0	A/5 21171	7.2500	NaN	S
+1	2	1	1	Cumings,Mrs. John Bradley (Florence Briggs Th...)	female	38.0	1	0	PC 17599	71.2833	C85	C
+"""
+
+
+if __name__ == '__main__':
+    pass
